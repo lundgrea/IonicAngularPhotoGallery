5064ad6ea42421ebc90575b67cc6f72e6f60d0d5
Alyssa Lundgren <lundgrea@gmail.com>
2020-01-16 09:05:06 -0700

    Add PWA imports into main.ts

--- src/test.ts
+++ src/test.ts
@@ -8,10 +8,6 @@ import {
 } from '@angular/platform-browser-dynamic/testing';
 
 
-import { defineCustomElements } from '@ionic/pwa-elements/loader';
-
-// Call the element loader after the platform has been bootstrapped
-defineCustomElements(window);
 
 declare const require: any;
 
